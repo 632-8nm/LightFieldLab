69183b70cd6e474faed25309f95253e2723b30b9
Transistor789 <1245545033@qq.com>
2025-04-04 00:45:55 +0800

    给refocus添加了一个新的功能

--- include/refocus.h
+++ include/refocus.h
@@ -2,10 +2,19 @@
 #define REFOCUS_H
 #include <QObject>
 #include <QTHread>
+#include <opencv2/core.hpp>
+#include <vector>
 class Refocus : public QObject {
 	Q_OBJECT
    public:
-	explicit Refocus(QObject* parent = nullptr);
+	explicit Refocus(QObject* parent = nullptr, int views = 0, int height = 0,
+					 int width = 0);
 	~Refocus();
+
+	cv::Mat refocus(const std::vector<cv::Mat>& input, float alpha, int offset);
+
+   private:
+	int		views_, height_, width_, len_, center_;
+	cv::Mat xgrid_, ygrid_;
 };
 #endif
--- src/refocus.cpp
+++ src/refocus.cpp
@@ -1,8 +1,47 @@
 #include "refocus.h"
 
-Refocus::Refocus(QObject* parent) : QObject(parent) {
+#include <opencv2/core/hal/interface.h>
+
+#include <cmath>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgproc.hpp>
+#include <vector>
+
+Refocus::Refocus(QObject* parent, int views, int height, int width)
+	: QObject(parent), views_(views), height_(height), width_(width) {
 	// Constructor implementation
+	len_ = std::sqrt(views);
+
+	xgrid_ = cv::Mat(height_, 1, CV_32F);
+	ygrid_ = cv::Mat(1, width_, CV_32F);
+	for (int x = 0; x < height_; x++) {
+		xgrid_.at<float>(x, 0) = static_cast<float>(x);
+	}
+	for (int y = 0; y < width_; y++) {
+		ygrid_.at<float>(0, y) = static_cast<float>(y);
+	}
+	xgrid_ = cv::repeat(xgrid_, 1, width_);
+	ygrid_ = cv::repeat(ygrid_, height_, 1);
 }
 Refocus::~Refocus() {
 	// Destructor implementation
+}
+cv::Mat Refocus::refocus(const std::vector<cv::Mat>& input, float alpha,
+						 int offset) {
+	cv::Mat output(input[0].size(), CV_32F, cv::Scalar(0));
+	float	factor = 1.0f / (1 - 1 / alpha);
+	for (int i = 0; i < input.size(); i++) {
+		int		row = i / len_;
+		int		col = i % len_;
+		cv::Mat temp;
+		cv::remap(input[i], temp, ygrid_ + factor * (row - center_),
+				  xgrid_ + factor * (col - center_), cv::INTER_LINEAR,
+				  cv::BORDER_CONSTANT, 0);
+		cv::Mat temp2;
+		cv::add(temp, output, output);
+	}
+	cv::divide((len_ - offset) * (len_ - offset), output, output);
+	output.convertTo(output, input[0].type());
+
+	return output;
 }
